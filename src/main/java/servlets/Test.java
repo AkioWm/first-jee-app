74ec8ce6598b7d6bc5ab1b5a1244cfb65a6139e8
AkioWm <cherif.kante16@gmail.com>
2024-02-22 19:07:27 +0100

    Create first Servlet

---
+++ src/main/java/servlets/Test.java
@@ -0,0 +1,27 @@
+package servlets;
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/Test")
+public class Test extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    public Test(){
+        super();
+    }
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+}
