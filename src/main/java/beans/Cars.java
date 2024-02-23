f770cbdb53609beb21f704ec562e9952ffdf0a39
AkioWm <cherif.kante16@gmail.com>
2024-02-23 17:07:15 +0100

    Create Cars bean

---
+++ src/main/java/beans/Cars.java
@@ -0,0 +1,37 @@
+package beans;
+
+public class Cars {
+    private String make;
+    private String model;
+    private boolean sportCar;
+
+    public Cars(){
+
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public boolean isSportCar() {
+        return sportCar;
+    }
+
+    public void setSportCar(boolean sportCar) {
+        this.sportCar = sportCar;
+    }
+
+
+}
